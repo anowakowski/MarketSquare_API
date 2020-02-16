66ef64e16934266e3d9b51fb9c38614f930ddcd4
anowakowski <anowakowski@mailc.net>
2020-02-16 21:53:07 +0100

    add sample comment

--- MarketSquare.API/Controllers/UsersController.cs
+++ MarketSquare.API/Controllers/UsersController.cs
@@ -14,6 +14,8 @@ namespace MarketSquare.API.Controllers
          [HttpGet("getAllUsers")]
         public async Task<IActionResult> GetAllUsers()
         {
+            // get users shouldbe from userServices with async
+            // sample data
             var users = new List<string>
             {
                 "test1",
