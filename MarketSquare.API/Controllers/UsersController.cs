8698ab164f3d1cc952563ee7c6fb3fecd8852d0a
anowakowski <anowakowski@mailc.net>
2020-02-16 20:52:30 +0100

    Added method for get all users

---
+++ MarketSquare.API/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MarketSquare.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+         [HttpGet("getAllUsers")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await GetAllUsers();
+
+            return Ok();
+        }
+    }
+}
--- MarketSquare.API/Data/Models/User.cs
+++ MarketSquare.API/Data/Models/User.cs
@@ -3,7 +3,6 @@ namespace MarketSquare.API.Data.Models
     public class User : Entity
     {
         public string Username { get; set; }
-
         public string Email { get; set; }
     }
 }
