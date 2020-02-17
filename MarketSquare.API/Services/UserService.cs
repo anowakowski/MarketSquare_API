1a0c8d5b76cf40edc68995d5e8d0f0a6680b7960
anowakowski <anowakowski@mailc.net>
2020-02-17 16:35:15 +0100

    use automapper in user services

--- MarketSquare.API/Services/UserService.cs
+++ MarketSquare.API/Services/UserService.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using MarketSquare.API.Data.Models;
 using MarketSquare.API.Data.Repositories;
+using AutoMapper;
+using MarketSquare.API.Dtos;
 
 namespace MarketSquare.API.Services
 {
     public class UserService : IUserService
     {
         private readonly IRepository<User> _userRepository;
-        public UserService(IRepository<User> userRepo)
+        private readonly IMapper _mapper;        
+        public UserService(IRepository<User> userRepo, IMapper mapper)
         {
             _userRepository = userRepo ?? throw new ArgumentNullException(nameof(userRepo));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
-        public async Task<IEnumerable<User>> GetUsers() 
+        public async Task<IEnumerable<UserForDetailedDto>> GetUsers() 
         {
-            return await _userRepository.GetAllAsync();
+            var users = await _userRepository.GetAllAsync();
+
+            var usersDto = _mapper.Map<UserForDetailedDto>(users);
+
+            return usersDto;
         }
     }
 }
